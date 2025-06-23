ccf302981c49c7b108dbf4260fa76e62535cea9d
taaha <taaha.s.bajwa@gmail.com>
2025-06-23 20:03:51 +0500

    update

--- src/app.py
+++ src/app.py
@@ -51,8 +51,11 @@ if prompt := st.chat_input(
 
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        response = process_async_response(
-            processed_prompt, st.session_state.agent_message_history
+        # response = process_async_response(
+        #     processed_prompt, st.session_state.agent_message_history
+        # )
+        response = st.write_stream(
+            run_mcp_client(processed_prompt, st.session_state.agent_message_history)
         )
 
     # Add assistant response to chat history
